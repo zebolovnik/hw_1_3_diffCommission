b0c1f4fca2aa799c72fb55337a67d32a972ae72c
Nik Ze <zebolovnik@yandex.ru>
2022-12-24 00:14:30 +1000

    Done

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,39 +1,5 @@
 package ru.netology
 
-/*
-fun main() {
-    val aaa = какятофункция(параметр1, параметр2, сПараметромПоУмолчанию2 = "silver"
-    println(aaa)
-}
-
-fun какятофункция(
-    параметр1: Int,
-    параметр2: String,
-    сПараметромПоУмолчанию1: Int = 100,
-    сПараметромПоУмолчанию2: String = "normal"
-    ): Int {
-        код-вычисления
-        return ...
-    }
-==========КОМИССИЯ==========
-Напишите алгоритм расчёта в виде функции, передавая в функцию:
-
-    * тип карты/счёта (по умолчанию VK Pay);
-    * сумму предыдущих переводов в этом месяце (по умолчанию 0 рублей);
-    * сумму совершаемого перевода.
-============================
-MasterCard
-Maestro
-VK Pay
-
-суммаПеревода
-всяСуммаЗаМесяц(MasterCard, Maestro) // должны сохранять каждый перевод по выбранной карте
-    // в МастерКард или Маэстро и когда привысится Лимит в календарный месяц 75тыс -> комиссия
-
-
- */
-
-
 fun main() {
     commissions(100_000, TYPE_MASTERCARD, 0)
     commissions(76000)
@@ -48,19 +14,13 @@ const val TYPE_VK = "VK Pay"
 
 fun commissions(
     transferAmount: Int, // сумма совершаемого перевода
-    typeCard: String = "VK Pay", // тип карты/счёта
+    typeCard: String = TYPE_VK, // тип карты/счёта
     amountPerMonth: Int = 0 // сумма всех переводов в этом месяце
 ) {
     val sum = amountPerMonth + transferAmount
     val calcCommission = transferAmount * (commissionByCardType(typeCard) / 100) // расчет суммы комиссии
     val resultCommission = if (sum > LIMIT_CARD && typeCard != TYPE_VK) calcCommission + 20 else 0.0
     return println("Сумма комиссии составит: ${resultCommission.toInt()} руб.")
-//    val resultCommission = when {
-////    val resultCommission = when (calcCommission > 0) {
-//        (transferAmount + amountPerMonth > LIMIT_CARD) -> calcCommission + 20
-//        else -> calcCommission
-//    }
-//    return println("Сумма комиссии составит: ${resultCommission.toInt()} руб.")
 }
 
 fun commissionByCardType(typeCard: String) = when (typeCard) {
